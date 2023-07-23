f555fe7b08a35ba173ba3f8b02f1ac685d3b9962
divyanshu <divyanshu.shahu@outlook.com>
2023-07-23 16:05:25 +0530

    newfiles

---
+++ provision-infra/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "res-gp" {
+  name = "app-grp"
+  location = "East US"
+  
+}
---
+++ provision-infra/variable.tf
@@ -0,0 +1,14 @@
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+  
+}
+
+variable "tenant_id" {
+  
+}
+variable "subscription_id" {
+  
+}
